466754d7e49f66a43722fad9630b70df82191ef6
Reiss Johnson <reissjohnson@me.com>
2016-03-02 18:52:11 +0000

    added relationship between reviews & restaurants, also implimented destroy dependency & test

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 feature 'restaurants' do  
-  
+
   context 'no restaurants have been added' do
-    
+
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content('no restaurants added yet')
@@ -12,20 +12,20 @@ feature 'restaurants' do
   end
 
   context 'restaurants have been added' do
-    
+
     before  do
       Restaurant.create(name: 'KFC')
     end
-    
+
     scenario 'adding a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'KFC'
       expect(page).not_to have_content 'no restaurants added yet'
     end
   end
-  
+
   context 'User can create a new restaurant' do
-    
+
     scenario 'prompts the user to fill out a form, then displays the restaurant' do
       visit '/restaurants'
       click_link 'Add a restaurant'
@@ -35,11 +35,11 @@ feature 'restaurants' do
       expect(current_path).to eq '/restaurants'
     end
   end
-  
+
   context 'Viewing restaurants' do
-    
+
     let!(:kfc) {Restaurant.create(name: 'KFC')}
-    
+
     scenario 'User can see restaurant\'s details' do
       visit '/restaurants'
       click_link 'KFC'
@@ -72,7 +72,5 @@ feature 'restaurants' do
       expect(page).not_to have_content 'KFC'
       expect(page).to have_content 'Restaurant deleted successfully' 
     end
-  end
-  
-
+ end
 end
