3fe5c3c9c54d2a56beb3318c9ed32ce4090dc561
Reiss Johnson <reissjohnson@me.com>
2016-03-02 23:39:11 +0000

    implimented function so a user cannot leave a review score of higher than 5

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,11 +2,20 @@ require 'rails_helper'
 
 describe Restaurant, type: :model do
   it { is_expected.to have_many(:reviews).dependent(:destroy) }
-end
-describe Restaurant, type: :model do
+
   it "is not a valid name if less than three characters" do
     restaurant = Restaurant.create(name: 'KF')
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
+
+  it "is not valid unless name is unique" do
+    Restaurant.create(name: 'bar1')
+    restaurant = Restaurant.new(name: 'bar1')
+    expect(restaurant).to have(1).error_on(:name)
+  end
 end
+
+
+
+
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 describe Review, type: :model do
   it { should belong_to :restaurant}
+
+  it "rating needs to be 5 or less" do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end
 end
