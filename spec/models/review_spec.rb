89babb005298a1fc8e50a8401f7c41cd8df10104
Reiss Johnson <reissjohnson@me.com>
2016-03-04 12:19:59 +0000

    added shoulda tests for associations of reviews/restaurants/ updated reviews table to include user_id foreign key

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe Review, type: :model do
   it { should belong_to :restaurant}
+  it { should belong_to :user }
 
   it "rating needs to be 5 or less" do
     review = Review.new(rating: 10)
